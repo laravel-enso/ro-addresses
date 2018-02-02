1825edbf3e5ec6f850f58f5e9db7b68a3fdb0e94
Mihai <mihai.ocneanu@gmail.com>
2018-02-02 14:38:54 +0200

    changed form items order; config cleanup

--- src/config/addresses.php
+++ src/config/addresses.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mihai
- * Date: 12/5/17
- * Time: 12:22 PM.
- */
 
 return [
 
     'addressables' => [
         'owner' => \App\Owner::class,
-        'user'  => \App\User::class,
     ],
     'streetTypes'  => [
         'Street'    => 'Street',
