6979f9e4a4263993a77c2d2ea3570a9f31ea8a54
Mihai <mihai.ocneanu@gmail.com>
2018-02-15 12:21:04 +0200

    small refactor/cleanup in the ro addresses wrapper

--- src/resources/assets/js/components/enso/addresses/RoAddresses.vue
+++ src/resources/assets/js/components/enso/addresses/RoAddresses.vue
@@ -6,46 +6,46 @@
             v-on="$listeners">
 
         <!--customized card content-->
-        <template slot="address" slot-scope="props">
-            <span v-if="props.address.street_type">{{ __(props.address.street_type) }}</span>
-            <span v-if="props.address.street">{{props.address.street}}</span>
-            <span v-if="props.address.number">{{__('Number')}}: {{ props.address.number }}</span>
+        <template slot="address" slot-scope="{ address }">
+            <span v-if="address.street_type">{{ __(address.street_type) }}</span>
+            <span v-if="address.street">{{address.street}}</span>
+            <span v-if="address.number">{{__('Number')}}: {{ address.number }}</span>
             <br>
-            <span v-if="props.address.building"><span class="has-text-grey">{{__(props.address.building_type)}} </span>{{ props.address.building }},</span>
-            <span v-if="props.address.entry">{{__('Entry')}}: {{ props.address.entry }}</span>
-            <span v-if="props.address.floor">{{__('Floor')}}: {{ props.address.floor }}</span>
-            <span v-if="props.address.apartment">{{__('Apartment')}}: {{ props.address.apartment }}</span>
+            <span v-if="address.building"><span class="has-text-grey">{{__(address.building_type)}} </span>{{ address.building }},</span>
+            <span v-if="address.entry">{{__('Entry')}}: {{ address.entry }}</span>
+            <span v-if="address.floor">{{__('Floor')}}: {{ address.floor }}</span>
+            <span v-if="address.apartment">{{__('Apartment')}}: {{ address.apartment }}</span>
             <br>
-            <span v-if="props.address.sub_administrative_area">{{__('Neighborhood')}}: {{ props.address.sub_administrative_area }}</span>
-            <span v-if="props.address.city">{{ props.address.city }},</span>
-            <span v-if="props.address.administrative_area">{{ props.address.administrative_area }}</span>
+            <span v-if="address.sub_administrative_area">{{__('Neighborhood')}}: {{ address.sub_administrative_area }}</span>
+            <span v-if="address.city">{{ address.city }},</span>
+            <span v-if="address.administrative_area">{{ address.administrative_area }}</span>
             <br>
-            <span v-if="props.address.postal_area">{{ props.address.postal_area }}</span>
+            <span v-if="address.postal_area">{{ address.postal_area }}</span>
             <br>
             <span class="icon">
                 <fa icon="globe"/>
             </span>
-            {{ props.address.country_name }} <br>
-            <span class="icon" v-if="props.address.obs">
+            {{ address.country_name }} <br>
+            <span class="icon" v-if="address.obs">
                 <fa icon="sticky-note"/>
             </span>
-            {{ props.address.obs }}
+            {{ address.obs }}
         </template>
 
         <!--custom form elements-->
-        <template slot="county_id" slot-scope="props">
+        <template slot="county_id" slot-scope="{ field, errors }">
             <vue-select name="county_id"
-                        v-model="props.field.value"
-                        @input="params.county_id=$event;props.errors.clear(props.field.name);"
-                        :options="props.field.meta.options">
+                        v-model="field.value"
+                        @input="params.county_id=$event;errors.clear(field.name);"
+                        :options="field.meta.options">
             </vue-select>
         </template>
-        <template slot="locality_id" slot-scope="props">
+        <template slot="locality_id" slot-scope="{ field, errors }">
             <vue-select name="locality_id"
                         :params="params"
-                        v-model="props.field.value"
-                        @input="props.errors.clear(props.field.name);"
-                        :source="props.field.meta.source">
+                        v-model="field.value"
+                        @input="errors.clear(field.name);"
+                        :source="field.meta.source">
             </vue-select>
         </template>
     </addresses>
@@ -55,7 +55,7 @@
 
 import { mapGetters } from 'vuex';
 import Addresses from './Addresses.vue';
-import VueSelect from '../vueforms/VueSelect.vue';
+import VueSelect from '../select/VueSelect.vue';
 
 export default {
     components: { Addresses, VueSelect },
