8ca1dc87345a7cbb34e45adf786d3ead917cd44e
mishu <mishu@xtelecom.ro>
2018-03-19 12:49:33 +0200

    updates for the new formbuilder

--- src/app/Models/Address.php
+++ src/app/Models/Address.php
@@ -2,14 +2,16 @@
 
 namespace LaravelEnso\RoAddresses\app\Models;
 
-class Address extends \LaravelEnso\AddressesManager\app\Models\Address
+use LaravelEnso\AddressesManager\app\Models\Address as Addresses;
+
+class Address extends Addresses
 {
-    private const RomaniaId = 184;
+    private const RomaniaCountryId = 184;
 
     protected $fillable = ['addressable_id', 'addressable_type', 'county_id', 'locality_id', 'type', 'is_default', 'apartment', 'floor', 'entry',
         'building_type', 'building', 'number', 'street', 'street_type', 'neighborhood', 'postal_area', 'obs', ];
 
-    protected $attributes = ['country_id' => self::RomaniaId];
+    protected $attributes = ['country_id' => self::RomaniaCountryId];
 
     protected $appends = ['city', 'administrative_area', 'country_name'];
 
--- src/resources/assets/js/components/enso/addresses/RoAddresses.vue
+++ src/resources/assets/js/components/enso/addresses/RoAddresses.vue
@@ -1,63 +1,64 @@
 <template>
 
-    <addresses
-            @form-loaded="setCountyId"
-            v-bind="$attrs"
-            v-on="$listeners">
-
-        <!--customized card content-->
+    <addresses @form-loaded="params.county_id = countyId($event)"
+        v-bind="$attrs"
+        v-on="$listeners">
         <template slot="address" slot-scope="{ address }">
-            <span v-if="address.street_type">
-                {{ __(address.street_type) }}
-            </span>
-            <span v-if="address.street">
-                {{ address.street }}
-            </span>
-            <span v-if="address.number">
-                {{ __('Number') }}: {{ address.number }}
-            </span>
-            <br>
-            <span v-if="address.building">
-                <span class="has-text-grey">
-                    {{ __(address.building_type) }}
+            <p>
+                <span v-if="address.street_type">
+                    {{ __(address.street_type) }}
                 </span>
-                {{ address.building }},
-            </span>
-            <span v-if="address.entry">
-                {{ __('Entry') }}: {{ address.entry }}
-            </span>
-            <span v-if="address.floor">
-                {{ __('Floor') }}: {{ address.floor }}
-            </span>
-            <span v-if="address.apartment">
-                {{ __('Apartment') }}: {{ address.apartment }}
-            </span>
-            <br>
-            <span v-if="address.sub_administrative_area">
-                {{ __('Neighborhood') }}: {{ address.sub_administrative_area }}
-            </span>
-            <span v-if="address.city">
-                {{ address.city }},
-            </span>
-            <span v-if="address.administrative_area">
-                {{ address.administrative_area }}
-            </span>
-            <br>
-            <span v-if="address.postal_area">
-                {{ address.postal_area }}
-            </span>
-            <br>
-            <span class="icon">
-                <fa icon="globe"/>
-            </span>
-            {{ address.country_name }} <br>
-            <span class="icon" v-if="address.obs">
-                <fa icon="sticky-note"/>
-            </span>
-            {{ address.obs }}
+                <span v-if="address.street">
+                    {{ address.street }}
+                </span>
+                <span v-if="address.number">
+                    {{ __('Number') }}: {{ address.number }}
+                </span>
+            </p>
+            <p>
+                <span v-if="address.building">
+                    <span class="has-text-grey">
+                        {{ __(address.building_type) }}
+                    </span>
+                    {{ address.building }},
+                </span>
+                <span v-if="address.entry">
+                    {{ __('Entry') }}: {{ address.entry }}
+                </span>
+                <span v-if="address.floor">
+                    {{ __('Floor') }}: {{ address.floor }}
+                </span>
+                <span v-if="address.apartment">
+                    {{ __('Apartment') }}: {{ address.apartment }}
+                </span>
+            </p>
+            <p>
+                <span v-if="address.sub_administrative_area">
+                    {{ __('Neighborhood') }}: {{ address.sub_administrative_area }}
+                </span>
+                <span v-if="address.city">
+                    {{ address.city }},
+                </span>
+                <span v-if="address.administrative_area">
+                    {{ address.administrative_area }}
+                </span>
+                <br>
+                <span v-if="address.postal_area">
+                    {{ address.postal_area }}
+                </span>
+            </p>
+            <p>
+                <span class="icon">
+                    <fa icon="globe"/>
+                </span>
+                {{ address.country_name }} <br>
+                <span class="icon" v-if="address.obs">
+                    <fa icon="sticky-note"/>
+                </span>
+                {{ address.obs }}
+            </p>
         </template>
 
-        <!--custom form elements-->
         <template slot="county_id" slot-scope="{ field, errors }">
             <vue-select name="county_id"
                 :label="field.meta.label || 'name'"
@@ -97,13 +98,11 @@ export default {
         ...mapGetters('locale', ['__']),
     },
     methods: {
-        getCountyId(form) {
-            const attribute = form.fields.find(attribute => attribute.name === 'county_id');
-
-            return attribute.value;
-        },
-        setCountyId(formData) {
-            this.params.county_id = this.getCountyId(formData);
+        countyId(form) {
+            return form.sections
+                .reduce((fields, section) => fields.concat(section.fields), [])
+                .find(attribute => attribute.name === 'county_id')
+                .value;
         },
     },
 };
