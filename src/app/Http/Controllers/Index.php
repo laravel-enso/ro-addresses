371c33a3cf6312272f386b1672f675680b51c82c
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2019-06-20 17:52:19 +0300

    properly imports address resource

--- src/app/Http/Controllers/Index.php
+++ src/app/Http/Controllers/Index.php
@@ -3,9 +3,9 @@
 namespace LaravelEnso\RoAddresses\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Resources\Json\Resource;
 use LaravelEnso\RoAddresses\app\Models\Address;
 use LaravelEnso\Addresses\App\Http\Requests\ValidateAddressFetch;
+use LaravelEnso\RoAddresses\app\Http\Resources\Address as Resource;
 
 class Index extends Controller
 {
